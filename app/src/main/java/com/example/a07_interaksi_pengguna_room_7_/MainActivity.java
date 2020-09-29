2fbae675c898aedfe2b8e7bced05c31d03c95bff
alfan06 <alfannoufal@gmail.com>
2020-09-29 11:14:55 +0700

    edit  UI

--- app/src/main/java/com/example/a07_interaksi_pengguna_room_7_/MainActivity.java
+++ app/src/main/java/com/example/a07_interaksi_pengguna_room_7_/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.a07_interaksi_pengguna_room_7_;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    String[] users = { "Teknik Informatika", "Teknik Sipil", "Teknik Mesin", "Teknik Kimia", "Akuntansi", "Teknik Elektro" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spin = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
+        Toast.makeText(getApplicationContext(), "Selected User: "+users[position] ,Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO - Custom Code
     }
 }
