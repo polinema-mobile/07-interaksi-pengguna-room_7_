e1e91673d0822c058a1205c677552f298327bcd5
alfan06 <alfannoufal@gmail.com>
2020-09-29 08:49:22 +0700

    UI Activity 1

---
+++ app/src/main/java/com/example/a07_interaksi_pengguna_room_7_/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a07_interaksi_pengguna_room_7_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
